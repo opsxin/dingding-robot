c55efe56f1e2ebb091c5f09f03d1a38d8a8f5f79
opsxin <haoxin0880@gmail.com>
2019-10-17 17:37:33 +0800

    格式化代码，从配置文件获取配置
    格式化
    
    添加 ActionCard 类型
    
    添加 FeedCard 类型
    
    修改 FeedCard
    
    添加缺少的 import sys
    
    修改格式
    
    从配置文件获取配置

--- dd_msg/markdown.py
+++ dd_msg/markdown.py
@@ -1,32 +1,90 @@
-"""Markdown 类型"""
-
+"""Markdown 类型
+"""
+import sys
 import json
 
 
 class MarkdownMsg(object):
     def __init__(self, title, content, at_mobile=[], at_all=False):
-        self.__msgtype = "markdown"
+        """初始化
+
+        :param title: 标题 
+        :param content: 消息内容
+        :param at_mobile: @ 用户
+        :param _at_all: @ 所有人
+        """
+        self._msgtype = "markdown"
         self.title = title
         self.content = content
         self.at_mobile = at_mobile
-        self.__at_all = at_all
-    
+        self._at_all = at_all
+
+    def mod_title(self, title):
+        """修改标题
+        """
+        if not title.strip():
+            self.title = title
+        else:
+            print("title 不能为空")
+
     def add_content(self, content):
-        self.content.append(content)
-    
+        """添加消息内容
+        """
+        self.content += content
+
     def del_content(self):
-        self.content.clear()
+        """清空消息内容
+        """
+        self.content = ""
+
+    def mod_content(self, content):
+        """修改消息内容
+        """
+        self.del_content()
+        self.add_content(content)
+
+    def add_at_num(self, num):
+        """添加手机号码
+        """
+        self.at_mobile.append(num)
+
+    def del_at_num(self, num):
+        """删除手机号码
+        """
+        try:
+            self.at_mobile.remove(num)
+        except ValueError:
+            print("删除的手机号码不存在")
 
     def set_at_all(self, at_values):
-        if type(at_values) == bool:
-            self.__at_all = at_values
+        """设置是否 @ 所有人
+        """
+        if isinstance(at_values, bool):
+            self._at_all = at_values
         else:
-            print("True 或 False")
+            print("set_at_all 的值只能 True 或 False")
 
     def conversion_json(self):
+        """转换内容为 JSON 格式
+        """
         if len(self.content) == 0:
             sys.exit("内容不能为空")
         else:
-            data = {'msgtype': self.__msgtype, 'markdown': {'text': ' '.join(self.content)
-                , 'title': self.title}, 'at': {'atMobiles': self.at_mobile, 'isAtAll': self.__at_all}}
+            data = {'msgtype': self._msgtype, 'markdown': {
+                'text': self.content, 'title': self.title}, 'at': {
+                'atMobiles': self.at_mobile, 'isAtAll': self._at_all
+                }
+            }
             return json.dumps(data)
+
+
+# 测试
+if __name__ == '__main__':
+    md = MarkdownMsg("杭州天气", "#### 杭州天气 @156xxxx8827\n")
+    md.add_content("> 9度，西北风1级，空气良89，相对温度73%\n\n")
+    md.del_content()
+    md.add_content("> ###### 10点20分发布 [天气](http://www.thinkpage.cn/) \n")
+    md.add_at_num("12345678910")
+    # md.del_at_num("12345678910")
+    md.set_at_all(True)
+    print(md.conversion_json())
--- dd_msg/text.py
+++ dd_msg/text.py
@@ -1,40 +1,88 @@
-"""Text 类型"""
+"""Text 类型
+"""
 import sys
 import json
 
 
 class TextMsg(object):
     def __init__(self, content, at_mobile=[], at_all=False):
-        self.__msgtype = "text"
+        """初始化
+
+        :param content: 消息内容
+        :param at_mobile: @ 用户
+        :param _at_all: @ 所有人
+        """
+        self._msgtype = "text"
         self.content = content
         self.at_mobile = at_mobile
-        self.__at_all = at_all
+        self._at_all = at_all
 
     def add_content(self, content):
-        self.content.append(content)
+        """添加消息内容
+
+        Args: 
+            content: 消息内容（String）
+        """
+        self.content += content
 
     def del_content(self):
-        self.content.clear()
+        """清空消息内容
+        """
+        self.content = ""
+    
+    def mod_content(self, content):
+        """修改消息内容
+        """
+        self.del_content()
+        self.add_content(content)
+
+    def add_at_num(self, num):
+        """添加手机号码
+
+        Args: 
+            num: 手机号码（String）
+        """
+        self.at_mobile.append(num)
+
+    def del_at_num(self, num):
+        """删除手机号码
+        """
+        try:
+            self.at_mobile.remove(num)
+        except ValueError:
+            print("删除的手机号码不存在")
 
     def set_at_all(self, at_values):
-        if type(at_values) == bool:
-            self.__at_all = at_values
+        """
+        设置是否 @ 所有人
+
+        Args: 
+            at_values（bool）
+        """
+        if isinstance(at_values, bool):
+            self._at_all = at_values
         else:
-            sys.exit("set_at_all 的值只能 True 或 False")
+            print("set_at_all 的值只能 True 或 False")
 
     def conversion_json(self):
+        """转换内容为 JSON 格式
+        """
         if len(self.content) == 0:
             sys.exit("内容不能为空")
         else:
-            data = {'msgtype': self.__msgtype, 'text': {'content': ' '.join(self.content)},
-                    'at': {'atMobiles': self.at_mobile}, 'isAtAll': self.__at_all}
+            data = {'msgtype': self._msgtype, 'text': {'content': self.content},
+                    'at': {'atMobiles': self.at_mobile}, 'isAtAll': self._at_all
+                    }
             return json.dumps(data)
 
 
 # 测试
-if __name__ == "__main__":
-    txt = TextMsg(["this is test text"])
-    txt.add_content("test 2")
-    txt.del_content()
-    txt.add_content("test 3")
-    txt.conversion_json()
+if __name__ == '__main__':
+    txt = TextMsg("测试发送 TXT 内容。")
+    txt.add_content("追加内容。")
+    # txt.del_content()
+    txt.add_content("设置新内容。")
+    txt.add_at_num("12345678910")
+    # txt.del_at_num("12345678910")
+    txt.set_at_all(True)
+    print(txt.conversion_json())
